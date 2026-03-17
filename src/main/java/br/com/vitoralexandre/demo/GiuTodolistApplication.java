7a553c8614bfd67c89346f885ab36a78530c1d36
Vitorlexandre <vitordhcosta333@gmail.com>
2026-03-17 13:26:46 -0300

    Subindo aplicação principal mais configurações do banco de dados de produção

---
+++ src/main/java/br/com/vitoralexandre/demo/GiuTodolistApplication.java
@@ -0,0 +1,13 @@
+package br.com.vitoralexandre.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiuTodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiuTodolistApplication.class, args);
+	}
+
+}
